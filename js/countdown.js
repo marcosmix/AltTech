afd86c173bc801dad8fe0e687f28a4ff729be68d
NEMESIS290DARK <125304954+NEMESIS290DARK@users.noreply.github.com>
2023-07-14 16:54:13 -0300

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -3,9 +3,9 @@
     simplyCountdown('#counter', {
         year: 2023, // required
         month: 8, // required
-        day: 5, // required
+        day: 4, // required
         hours: 9, // Default is 0 [0-23] integer
-        minutes:0, // Default is 0 [0-59] integer
+        minutes: 0, // Default is 0 [0-59] integer
         seconds: 0, // Default is 0 [0-59] integer
         words: { //words displayed into the countdown
             days: { singular: 'Día', plural: 'Días' },
@@ -35,3 +35,4 @@ simplyCountdown(myElement, { /* options */ });
 
 let multipleElements = document.querySelectorAll('.my-countdown');
 simplyCountdown(multipleElements, { /* options */ });
+
